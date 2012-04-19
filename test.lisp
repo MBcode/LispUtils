de92628fe93801a08dac7bffc64ee49cc1ba83ce
MBcode <mike.bobak+git@gmail.com>
2012-04-19 15:19:56 -0700

    used test words

--- test.lisp
+++ test.lisp
@@ -1,14 +1,23 @@
 ;;;;;;a test use of util_mb.lisp
 ;;=======================================> play.lisp <==
 ;all the strategy is here
-;(defvar *words* (map-lines "w10" #'(lambda (wl) (intern (string-upcase wl))))) ;change to w10
-(defvar *words* (map-lines "w10" #'(lambda (wl) (string-upcase wl)))) ;regex this/?
-(defvar *wordls* (map-lines "w10" #'(lambda (wl) (explode2s (string-upcase wl))))) 
-(defvar *alphaw10* '(E S I R A N T O L C D U G M P H B Y F V K W Z X Q J)) ;can gen from init words /ofCurrentLen
-                      ;from http://www.datagenetics.com/blog/april12012/index.html
+;;(defvar *words* (map-lines "w10" #'(lambda (wl) (intern (string-upcase wl))))) ;change to w10
+;(defvar *words* (map-lines "w10" #'(lambda (wl) (string-upcase wl)))) ;regex this/?
+;(defvar *wordls* (map-lines "w10" #'(lambda (wl) (explode2s (string-upcase wl))))) 
+(defvar *wordls* nil)
+(defvar *alphaw10* '(E S I R A N T O L C D U G M P H B Y F V K W Z X Q J)) ;use4now/finish
+;can gen from init words /ofCurrentLen ;from http://www.datagenetics.com/blog/april12012/index.html
 ;can index all/some or since will re-get freq just go down list till contin2fall4a bit
 ;set_diff&implode2s added2utils   ;set_diff =complement?
 ;s3/pdt stuff in p1.cl, use s4 now
+;-
+(defun useWordsOfLen (n)  
+  "set once know length of word using"
+  (len 
+   (setf *wordls* (map-lines (str-cat "w" n) #'(lambda (wl) (explode2s (string-upcase wl)))))))
+(defun initGame (cur)  
+  (let ((cl (len cur)))
+    (format t "~%Have a game of ~a letters,~a~%" cl (useWordsOfLen cl))))
 ;-
 (defvar *lg* nil)
 (defvar *lm* nil)
@@ -39,6 +48,7 @@
 (defvar *picked* '()) 
 (defun suggest (current) ;includes len, which is set to 10 on 1st pass
   "given current's constraints, find max occurance of possible words"
+  (unless *wordls* (initGame current))
   (let* ((pick-l (set_diff  *alphaw10* *picked*)) ;lst2pick from&use2get freq/max-let-occur
         ;(pick (first pick-l)) ;will reorder by count of each in cur wordls
          (missed (set_diff *picked* current)) ;use2fileter wordls
@@ -76,7 +86,9 @@
     (format t "~%m:~a,lm:~a,g:~a,lg:~a,~a,~a,suggest(~a)4:~a" 
             missed lm got lg lgp (len nwl) pick current)
     (setf *lm* missed)
-    (setf *lg* got))) 
+    (setf *lg* got))
+  )
+
 ;;=======================================> hang.lisp <==
 ;a quick hangman game to play; start w/1test word and have play/strategy give suggestions; then expand MB
 ;status{GAME_WON, GAME_LOST, KEEP_GUESSING}
@@ -127,3 +139,32 @@
                  nil)))))
 
 (defun tst2 () (hangman '(r e m e m b e r e d))) ;in 5 letter trys &default in 10
+
+;;=======================================> tst.lisp <==
+(defvar *tsts* '(;my-score in comments sometimes doing better than original game maker's
+ ("COMAKER" 25) ;14  ;(was not able to guess the word before making more than 5 mistakes)
+ ("CUMULATE" 9) ;8
+ ("ERUPTIVE" 5) ;9
+ ("FACTUAL" 9)  ;9
+ ("MONADISM" 8) ;11
+ ("MUS" 25) ;9 ;(was not able to guess the word before making more than 5 mistakes)
+ ("NAGGING" 7) ;7
+ ("OSES" 5)    ;3
+ ("REMEMBERED" 5) ;5
+ ("SPODUMENES" 4) ;10
+ ("STEREOISOMERS" 2) ;7
+ ("TOXICS" 11) ;11
+ ("TRICHROMATS" 5) ;10
+ ("TRIOSE" 5) ;7
+ ("UNIFORMED" 5))) ;13 
+
+(defun hang ()
+  "test hangman"
+  (format t "~%TestHangman w/word or nth of~%~a" *tsts*)
+  (let* ((choice (read))
+         (word (if (numberp choice) 
+                 (let ((cp  (nth (min choice (len *tsts*)) *tsts*)))
+                   (format t "~%Try to get it in:~a~%" (second cp))
+                   (first cp))
+                 (string choice))))
+    (hangman (explode2s word))))
