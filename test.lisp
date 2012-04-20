eac45feb9e4adb174b9c3d7a08c5418156f5a3a9
MBcode <mike.bobak+git@gmail.com>
2012-04-20 14:06:03 -0700

    dbg off

--- test.lisp
+++ test.lisp
@@ -1,5 +1,5 @@
 (load "util_mb") ;;;a test use of util_mb.lisp
-(defvar *dbg* t)
+(defvar *dbg* nil)
 (defvar *auto* t)
 ;;============================================> play.lisp <==
 ;all the strategy is here
@@ -7,9 +7,7 @@
 (defvar *alphaw10* '(E S I R A N T O L C D U G M P H B Y F V K W Z X Q J)) ;use4now/finish
 (defvar *alphaw-n* nil)
 ;can gen from init words /ofCurrentLen ;from http://www.datagenetics.com/blog/april12012/index.html
-;can index all/some or since will re-get freq just go down list till contin2fall4a bit
 ;set_diff&implode2s added2utils   ;set_diff =complement?
-;s3/pdt stuff in p1.cl, use s4 now
 ;-
 (defvar *lg* nil)
 (defvar *lm* nil)
@@ -87,11 +85,11 @@
   (when nwl 
     (if (len-gt nwl 1) 
       (progn (setf *wordls* nwl)
-             (format t "~%try:~a" pick)
+             (when *dbg* (format t "~%try:~a" pick))
              ) ;else only 1 word left so pick it
       (progn (setf pick (implode2s (first-lv nwl)))
              (format t "~%GUESS:~a" nwl)))
-    (format t "~%wl~a->~a,~a" wll (len *wordls*) (head *wordls* 6))
+    (when *dbg* (format t "~%wl~a->~a,~a" wll (len *wordls*) (head *wordls* 6)))
     ;now get max occur letter, &suggest that now
     )
     (when *dbg*
@@ -102,58 +100,60 @@
   pick))
 
 ;;============================================> hang.lisp <==
-;a quick hangman game to play; start w/1test word and have play/strategy give suggestions; then expand MB
+;a quick hangman game to play;start w/1test word and have play/strategy give suggestions; then expand MB
 ;status{GAME_WON, GAME_LOST, KEEP_GUESSING}
 ;load: uts.cl or util_mb.lisp
-;when I started to rewrite the game I got rid of things the game didn't use; &only have it in player.lisp now
+;when I started to rewrite the game I got rid of things the game didn't use;&only have it in player.lisp now
 ;started from: http://lyle.smu.edu/~mhd/5320sp02/hang.lisp
 (defun update-word (word current guess)
    "Update the current guess.  We can assume that the input is always a list."
    (let ((found nil))
-      (if  (and (len-gt guess 1) (equal (explode2s guess) word)) 
+      (if (and (len-gt guess 1) (equal (explode2s guess) word)) 
         (progn (when *dbg* (format t "~%GOT IT HERE~%"))
                (setf current (explode2s guess))
-               (setf found t))
-      (dotimes (i (length word) current) ; Word and Current have the same length
-         (if (equal (nth i word) guess)
-           (progn (setf found t) (setf (nth i current) guess))
-             nil)))
-      (if found (format t "Good try, keep guessing")
-                (format t "Nope, sorry~%"))
+               (setf found 'GAME_WON))
+        (dotimes (i (length word) current) ; Word and Current have the same length
+          (if (equal (nth i word) guess)
+            (progn (setf found 'KEEP_GUESSING) (setf (nth i current) guess))
+            nil)))
+      (if found (when (eq found 'KEEP_GUESSING) (format t "~a Good try, keep guessing" guess))
+                (format t "~a Nope, sorry~%" guess))
       current))
 
 
 (defun hangman (&optional (word  '(a r t i f i c i a l )) (mxWg 4))
    "Simple hangman game.  The player has to keep track of letters he/she has already guessed."
    (let* ((maxtries 15)
-         (mx-current '(- - - - - - - - - - - - - - - - - - - - - - - - -)) 
-         (current (subseq mx-current 0 (len word)))
-         (letterGuesses 0)
-         (wordGuesses 0) ;if this maxes then letterGuesses goes to 25
-         (guess nil))
+          (mx-current '(- - - - - - - - - - - - - - - - - - - - - - - - -)) 
+           (current (subseq mx-current 0 (len word)))
+          (letterGuesses 0)
+          (wordGuesses 0) ;if this maxes then letterGuesses goes to 25
+          (guess nil))
       (dotimes (i maxtries)
         (let ((sug (suggest current))) ;does: (print current)
+          (unless *dbg* (print current)) ;unless dbg off
          (terpri)
          (format t "What is your guess?  ")  ;start by calling play.lisp to get a suggestion
          (setf guess 
                (if *auto* sug
                  (read)))
          (terpri)
-         (setf current (update-word word current guess))
-         (if (equal word current)  ;put let/word guess logic into update ;it is cleaner there
-         ;if (or (if (and (eq (len guess) 1) (equal word current)) t (progn (incf letterGuesses) nil))
-         ;       (if (equal word (explode2s guess)) t (progn (incf wordGuesses) 
-         ;                                          (when (>= wordGuesses mxWg) (setf letterGuesses 25))
-         ;                                                                     nil)))
-             (progn                                    ;   Here is something new
-                (print current)
-                (terpri)
-                ;(format t "Congratulations!  You have won the game.~%")
-                (format t "Congratulations!  You found ~a in ~a,~a,~a~%" word (- 25 maxtries) letterGuesses wordGuesses)
+         (if (len-gt guess 1)  ;incr word or letter guess count
+           (progn (incf wordGuesses)
+                  (when (> wordGuesses mxWg) (setf letterGuesses 25)
+                    (when *dbg* (format t "~%TooManyWordGuesses~%"))))
+           (incf letterGuesses)) 
+         (setf current (update-word word current guess)) ;update/check if correct
+         (if (equal word current)  
+             (progn (print current) (terpri)
+               ;(format t "Congratulations!  You have won the game:~a~%" word)
+                (format t "Congratulations!  You have won the game w/~a in:~a+~a=~a~%" 
+                        word letterGuesses wordGuesses (+ letterGuesses wordGuesses))
                 (return nil))  ;guess letter/word in fncs &ret status2use here
-             (if (equal i (- maxtries 1))
-                 (format t "You lost this time.  Try again!")
-                 nil))))))
+             (if (or (>= letterGuesses 25) (> wordGuesses mxWg)) ;only have to test for 1st
+                 (format t "You lost this time:~a,~a  Try again!" letterGuesses wordGuesses)
+                 nil) ;why nil
+             )))))
 
 (defun tst2 () (hangman '(r e m e m b e r e d))) ;in 5 letter trys &default in 10
 
@@ -163,17 +163,17 @@
  ("CUMULATE" 9) ;8
  ("ERUPTIVE" 5) ;9
  ("FACTUAL" 9)  ;9
- ("MONADISM" 8) ;8
+ ("MONADISM" 8) ;9
  ("MUS" 25) ;9 ;(was not able to guess the word before making more than 5 mistakes)
  ("NAGGING" 7) ;7
  ("OSES" 5)    ;3
  ("REMEMBERED" 5)   ;5
  ("SPODUMENES" 4)   ;5
- ("STEREOISOMERS" 2);7
+ ("STEREOISOMERS" 2);3
  ("TOXICS" 11)      ;11
  ("TRICHROMATS" 5)  ;6
  ("TRIOSE" 5)       ;7
- ("UNIFORMED" 5)))  ;9 
+ ("UNIFORMED" 5)))  ;13 
 
 (defun hang ()
   "test hangman"
