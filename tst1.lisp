519d36b533b4c1335b607aa3d4c7d1411fdcdd02
MBcode <mike.bobak+git@gmail.com>
2012-05-02 23:53:02 -0700

    time2refill for any n prods

--- tst1.lisp
+++ tst1.lisp
@@ -21,9 +21,12 @@
 
 ;;; Start w/1product and one timeline since the last restock, @each use have a time-till-out estimate
 ;;; factor in restock time, and w/other products might bundle but if from diff vendors can ingnore
-
+(defvar *restockT* 50) ;in hrs  ;could vary per product
 ;;; in ins will update burnRate, but should keep the one to generate data seperate  ;;assume persists
 (defvar *prods* '((p1 800 0.5) (p2 700 0.6) (p3 750 0.45) (p4 500 0.4)))  ;init for generation
+(defun gen-prods (n) 
+  "make any number of fake product in the store, burn-rate used for fake usage data"  ;test this1
+  (setf *prods* (loop for p from 1 to n collect (list (str-cat "p" n) (+ 700 (rn 200)) (/ (rn) 10.0))))) 
 
 ;instance will have lastRestockTime(as start), that stockingLevel, & (restock)R-eta which will be put in terms of days out from present time
 ;sL - burnRate * time = timeOut
@@ -43,21 +46,14 @@
         (m (third p-s-m)))
     (loop for h from 2 to 700 by 8 collect 
           (list  (floor (rn h 2))  (floor (rn (curStock-s-m-h s m h)))))))
-(defvar *p-utslr* (mapcar #'gen-prod-data *prods*))
+(defvar *p-utslr* (mapcar #'gen-prod-data *prods*))  ;can turn into a set of instances, but not necc yet
 (defvar *p1-utslr* '( ;could have drop a little faster, but fine for now ;;after this gen each time for each of *prods*
- (1 794) (9 799) (18 790) (27 792) (35 783) (41 783) (49 779) (58 770) (64 770)
- (72 768) (79 758) (90 747) (99 746) (105 733) (113 738) (120 737) (128 730)
- (135 728) (142 731) (154 720) (158 721) (168 718) (176 703) (184 702)
- (189 703) (203 702) (213 685) (218 687) (221 683) (234 679) (241 671)
- (249 679) (256 675) (268 665) (276 664) (278 654) (289 657) (297 646)
- (307 650) (312 647) (322 642) (330 638) (339 633) (344 627) (352 627)
- (362 619) (372 612) (382 610) (385 607) (391 606) (397 598) (404 593)
- (416 591) (425 584) (432 586) (444 578) (453 577) (454 571) (463 571)
- (473 556) (484 563) (492 555) (493 549) (505 544) (511 542) (522 538)
- (531 538) (539 534) (546 523) (555 526) (560 520) (571 519) (579 504)
- (582 506) (593 504) (601 500) (611 489) (615 485) (626 486) (633 480)
- (644 483) (650 474) (657 473) (665 455) (674 461) (680 453) (692 455)
- (692 451)) 
+ (1 794) (9 799) (18 790) (27 792) (35 783) (41 783) (49 779) (58 770) (64 770) (72 768) (79 758) (90 747) (99 746) (105 733) (113 738) (120 737) (128 730)
+ (135 728) (142 731) (154 720) (158 721) (168 718) (176 703) (184 702) (189 703) (203 702) (213 685) (218 687) (221 683) (234 679) (241 671)
+ (249 679) (256 675) (268 665) (276 664) (278 654) (289 657) (297 646) (307 650) (312 647) (322 642) (330 638) (339 633) (344 627) (352 627)
+ (362 619) (372 612) (382 610) (385 607) (391 606) (397 598) (404 593) (416 591) (425 584) (432 586) (444 578) (453 577) (454 571) (463 571)
+ (473 556) (484 563) (492 555) (493 549) (505 544) (511 542) (522 538) (531 538) (539 534) (546 523) (555 526) (560 520) (571 519) (579 504)
+ (582 506) (593 504) (601 500) (611 489) (615 485) (626 486) (633 480) (644 483) (650 474) (657 473) (665 455) (674 461) (680 453) (692 455) (692 451)) 
 )
 (defun ff (n) (* 1.0 (floor n)))
 ;or(loop for h from 2 to 700 by 8 collect (list (ff (rn h 2))  (ff (rn (curStock-h h))))) 
@@ -67,7 +63,12 @@
 (defun t1 () (statistics:x-int *p1-utslr*))
 (defun tn (n) (nth n *p-utslr*))
 (defun tx (n) (* 1.0 (statistics:x-int (tn n))))
+(defun tr (n) 
+  (let ((tr (- (tx n) *restockT*)))
+    (format t "~%~a hrs till need to refill" (floor tr))
+    tr))
 
+;pull in data-lib so everything doesn't have to be hrs since last restock
 #+ignore ;in statistics pkg right now
 (defun lin-regression (points) ;a version of lin-regression (points)
   (test-variables (points sequence))
@@ -112,18 +113,22 @@
 ;USER(2): (* 1.0 *)
 ;1597.7573
 ;USER(3): 
-;==now can guess when (in hrs) that you will run out of any number(presently 4)of products
-;USER(1): (tx 0) 
+;==now can guess when (in hrs) that you will run out of any number(presently 4)of products, but can do any#
+;USER(1): (tr 0)
 ;Intercept = 800.5004, slope = -0.50247264, x-int = 1593.1224, r = -0.99915904, R^2 = 0.9983187
-;1593.1224
-;USER(2): (tx 1) 
+;1543 hrs till need to refill
+;1543.1224
+;USER(2): (tr 1)
 ;Intercept = 698.6234, slope = -0.5977067, x-int = 1168.8398, r = -0.9995435, R^2 = 0.9990873
-;1168.8398
-;USER(3): (tx 2) 
+;1118 hrs till need to refill
+;1118.8398
+;USER(3): (tr 2)
 ;Intercept = 749.0641, slope = -0.44990206, x-int = 1664.9492, r = -0.9991216, R^2 = 0.99824387
-;1664.9492
-;USER(10): (tx 3) 
+;1614 hrs till need to refill
+;1614.9492
+;USER(4): (tr 3)
 ;Intercept = 499.9664, slope = -0.40089267, x-int = 1247.1328, r = -0.9985216, R^2 = 0.9970454
-;1247.1328
-;USER(4): 
+;1197 hrs till need to refill
+;1197.1328
+;USER(5): 
  
