ee569c50debfd93e0908cb9f48ed7854ec1b9d85
MBcode <bobak@balisp.org>
2016-11-30 00:06:09 -0800

    exmpl use of tsh

--- ts.lisp
+++ ts.lisp
@@ -14,3 +14,16 @@
 (defun run-sh (&rest strs)  
   "easier replacement for run-ext"
   (tsh (str-cat strs))) 
+
+;-
+;use tsh for some quick scripting
+(defun split-header (fn &optional (splt #\tab))
+  (split-string (tsh (format nil "head -1 ~a" fn)) splt))
+
+(defun cut-fn-nth-u (fn n)
+  (numstr (tsh (format nil "cut -f~d ~a|sort -u|wc" n fn))))
+
+(defun sv-unique-alst (fn &optional (splt #\tab)) ;tsv or #\, for csv
+  (let* ((colNames (split-header fn splt))
+         (numUnique (loop for i from 1 upto (len colNames) collect (cut-fn-nth-u fn i))))
+    (mapcar #'cons colNames numUnique)))
