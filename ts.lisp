4c86c0170d38b80b7b5586d000fa1428719ee694
MBcode <bobak@balisp.org>
2016-12-02 12:53:48 -0800

    ex:ratio uniq in col

--- ts.lisp
+++ ts.lisp
@@ -15,8 +15,7 @@
   "easier replacement for run-ext"
   (tsh (str-cat strs))) 
 
-;-
-;==use tsh for some quick scripting
+;use tsh for some quick scripting
 ;-
 ;-tsv/csv distribution of unique vals per col
 
@@ -25,11 +24,29 @@
 
 (defun cut-fn-nth-u (fn n)
   (numstr (tsh (format nil "cut -f~d ~a|sort -u|wc" n fn)))) ;or 'uniq' vs sort -u
+ ;cut -f n  of filename fn
+(defun cut-fn-nth-u-wc (fn n)
+  (numstr (tsh (format nil "cut -f~d ~a|uniq|wc" n fn))))
+(defun cut-fn-nth-wc (fn n)
+  (numstr (tsh (format nil "cut -f~d ~a|wc" n fn))))
+(defun cut-fn-nth-wcr (fn n)
+  "ratio of uniq/all in the column"
+  (/ (cut-fn-nth-u-wc fn n) (cut-fn-nth-wc fn n)))
 
-(defun sv-unique-alst (fn &optional (splt #\tab)) ;tsv or #\, for csv
+
+;defun sv-unique-alst (fn &optional (splt #\tab)) ;tsv or #\, for csv
+(defun sv-cutfnc-alst (fn &optional (splt #\tab) (cutfnc #'cut-fn-nth-u)) ;tsv or #\, for csv
   (let* ((colNames (split-header fn splt))
-         (numUnique (loop for i from 1 upto (len colNames) collect (cut-fn-nth-u fn i))))
+        ;(numUnique (loop for i from 1 upto (len colNames) collect (cut-fn-nth-u fn i)))
+         (numUnique (loop for i from 1 upto (len colNames) collect (funcall cutfnc fn i)))
+         )
     (mapcar #'cons colNames numUnique)))
+
+(defun sv-unique-alst (fn &optional (splt #\tab)) 
+   (sv-cutfnc-alst fn splt #'cut-fn-nth-u-wc))
+(defun sv-uniq-r-alst (fn &optional (splt #\tab)) 
+   (sv-cutfnc-alst fn splt #'cut-fn-nth-wcr))
+
 ;-
 ;-save url text in file named after url
 
@@ -49,4 +66,3 @@
 (defun url2txt-file (url)
   (let ((fn (str-cat (url2fn url) ".txt")))
     (str2file (url2txt url) fn)))
-;- 
